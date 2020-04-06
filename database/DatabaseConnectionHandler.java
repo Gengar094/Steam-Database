3f15704b02bfb833034a14a01c70bd4967734b1a
Yizhou Shen <harry8220@outlook.com>
2020-04-05 17:43:29 -0700

    add modifyProfile()

--- database/DatabaseConnectionHandler.java
+++ database/DatabaseConnectionHandler.java
@@ -370,6 +370,59 @@ public class DatabaseConnectionHandler {
 		return rs;
 	}
 
+	public void modifyProfile(String playerID, String field, String value) {
+		try {
+			switch (field) {
+				case "pname": {
+					PreparedStatement ps = connection.prepareStatement("UPDATE player SET pname = ? WHERE player_id = ?");
+					ps.setString(1, value);
+					ps.setString(2, playerID);
+
+					int rowCount = ps.executeUpdate();
+					if (rowCount == 0) {
+						System.out.println(WARNING_TAG + " PlayerID " + playerID + " does not exist!");
+					}
+
+					connection.commit();
+
+					ps.close();
+				}
+				case "email": {
+					PreparedStatement ps = connection.prepareStatement("UPDATE player SET email = ? WHERE player_id = ?");
+					ps.setString(1, value);
+					ps.setString(2, playerID);
+
+					int rowCount = ps.executeUpdate();
+					if (rowCount == 0) {
+						System.out.println(WARNING_TAG + " PlayerID " + playerID + " does not exist!");
+					}
+
+					connection.commit();
+
+					ps.close();
+				}
+				case "city": {
+					PreparedStatement ps = connection.prepareStatement("UPDATE player SET city = ? WHERE player_id = ?");
+					ps.setString(1, value);
+					ps.setString(2, playerID);
+
+					int rowCount = ps.executeUpdate();
+					if (rowCount == 0) {
+						System.out.println(WARNING_TAG + " PlayerID " + playerID + " does not exist!");
+					}
+
+					connection.commit();
+
+					ps.close();
+				}
+
+			}
+		} catch (SQLException e) {
+			System.out.println(EXCEPTION_TAG + " " + e.getMessage());
+			rollbackConnection();
+		}
+	}
+
 	public void deleteBranch(int branchId) {
 		try {
 			PreparedStatement ps = connection.prepareStatement("DELETE FROM branch WHERE branch_id = ?");
